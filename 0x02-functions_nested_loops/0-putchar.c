460a01e84580ece234cd2a6a6b911c8f24eb62b0
Ndak Syermen Sophie <syermenndak@gmail.com>
2023-02-21 14:27:21 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * A program that prints _putchar,
+ * followed by a new line.
+ * And return:0
+ */
+
+int main(void)
+{
+char c[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
