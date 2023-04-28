5712cb947d158243203e7e0b29816f225f94b62a
Agmada <syermenndak@gmail.com>
2023-04-28 07:02:32 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists."
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strlen - finds the length of a string
+ * @str: string to find the length of
+ *
+ * Return: length of string
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	size_t p;
+	list_t *new;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	return (NULL);
+	new->str = strdup(str);
+	for (p = 0; str[p]; p++)
+		new->len = _strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
