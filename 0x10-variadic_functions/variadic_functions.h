8608a4f90de2508d9b98eb3f28d582305122a264
Agmada <syermenndak@gmail.com>
2023-04-28 04:48:27 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef VARADIC_FUNCTION_H
-#define VARADIC_FUNCTION_H
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
