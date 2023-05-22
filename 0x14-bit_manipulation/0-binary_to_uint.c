0bb228c1b0f4fc9561633847a7fd330ac507b5db
Agmada <syermenndak@gmail.com>
2023-05-22 03:53:43 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,30 @@
 #include "main.h"
+
 //
-//binary_to_uint - converts to an unsigned int
-//from a binary number.
-//@b: binary
-//Return: unsigned int.
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
 //
 
-unsigned int binary_to_uint(const char *b)
-{
-unsigned int pen;
-int p, base_two;
-
-if (!b)
-pen = 0;	
-	return (0);
-
- for (p = 0; b[p] != '\0'; p++);
- for (p--, base_two = 1; p >= 0; p--, base_two *= 2)
- {
- if (b[p] != '0' && b[p] != '1')
- {
- return (0);
+ unsigned int binary_to_uint(const char *b){
+ 	unsigned int num;
+	int p;
+	
+	if (!b)
+	num = 0;
+		return (0);
+	for (p = 0; b[p] != '\0'; = p++)
+	{
+	if (b[p] != '0' && b[p] != '1')
+		return (0);
+	}
+	for (p = 0; b[p] != '\0'; p++)
+	{
+	num <<= 1;
+	if (b[p] == '1')
+		num += 1;
+	}
+ 	return (num);
  }
- if (b[p] & 1)
- {
- pen += base_two;
- }
-
- }
-
- return (pen);
-}
