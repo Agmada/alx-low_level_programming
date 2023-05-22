dcc614b3c0bff7ef67616ea4937d7616aec82b6e
Agmada <syermenndak@gmail.com>
2023-05-22 06:09:49 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,24 +9,25 @@
  *
  * Return: unsigned int.
  *
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, mult = 1;
+	int len;
 
- unsigned int binary_to_uint(const char *b){
- 	unsigned int num;
-	int p;
-	
-	if (!b)
-	num = 0;
+	if (b == '\0')
 		return (0);
-	for (p = 0; b[p] != '\0'; = p++)
+
+	for (len = 0; b[len];)
+		len++;
+
+	for (len -= 1; len >= 0; len--)
 	{
-	if (b[p] != '0' && b[p] != '1')
-		return (0);
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		num += (b[len] - '0') * mult;
+		mult *= 2;
 	}
-	for (p = 0; b[p] != '\0'; p++)
-	{
-	num <<= 1;
-	if (b[p] == '1')
-		num += 1;
-	}
- 	return (num);
- }
+
+	return (num);
+}
